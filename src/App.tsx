e207d8ced8d004bd1162073e667f4ad28183f79f
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-25 21:24:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Layout from "./Layout"; 
-import Home from "./page/Home"; 
+import Home from "./page/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <Router>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} /> {/* Should render Home inside Layout */}
-          </Route>
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Directly render Home */}
+      </Routes>
     </Router>
   );
 }
