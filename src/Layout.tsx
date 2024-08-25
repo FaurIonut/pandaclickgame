2cae4cca2e38fc809661154568b5300ee0cb5e5a
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-25 21:40:43 +0300

    .

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,6 @@
 import { Outlet } from "react-router-dom";
-
 export default function Layout() {
   return (
-    <div>
-      {/* Any static layout content can go here */}
-      <Outlet /> {/* This will render the Home component or any child routes */}
-    </div>
+      <Outlet />
   );
 }
