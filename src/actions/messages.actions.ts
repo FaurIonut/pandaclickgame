30754c36d365ecdb315890bef99fb706486524b1
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-26 01:31:16 +0300

    .

--- src/actions/messages.actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { Bot } from "grammy";
-
-const bot = new Bot(process.env.TELEGRAM_BOT_API_KEY!);
-
-export async function sendMessage({ chatId }: { chatId: string }) {
-  bot.api.sendMessage(chatId, "Sample message");
-}
--- src/actions/stats.actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import prisma from "@/lib/prisma";
-
-export async function appStats() {
-  const points = await prisma.user.aggregate({ _sum: { points: true } });
-  console.log({ allPoints: points._sum.points });
-}
