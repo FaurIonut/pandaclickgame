4f927f7b0326a95508f49b70d930280969975dea
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-25 22:52:33 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import WebApp from '@twa-dev/sdk';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import WebApp from "@twa-dev/sdk";
 
-import './index.css';
+import "./index.css";
 
-function Root() {
-  const [isWebAppReady, setIsWebAppReady] = useState(false);
+WebApp.ready();
 
-  useEffect(() => {
-    WebApp.ready().then(() => setIsWebAppReady(true));
-  }, []);
-
-  if (!isWebAppReady) {
-    return <div>Loading...</div>; // Optionally, a placeholder while WebApp is initializing
-  }
-
-  return (
-    <TonConnectUIProvider manifestUrl="https://109.237.99.151:3000/tonconnect-manifest.json">
-      <App />
-    </TonConnectUIProvider>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<Root />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl="https://109.237.99.151:3000/tonconnect-manifest.json">
+    <App />
+  </TonConnectUIProvider>
+);
