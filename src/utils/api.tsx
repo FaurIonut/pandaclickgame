dfa14dd799ed9626f735d8e7210a17449a0b2b56
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-26 00:16:00 +0300

    .

--- src/utils/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const api = axios.create({
-    baseURL: `https://grizzled-even-sociology.glitch.me/api`,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-
-  export default api;
