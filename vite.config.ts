6c0ff5879e0d49669a26e619ac8a563ee6dc3bda
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-25 22:55:46 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Specify the output directory
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        // Configure manual chunking to optimize large bundles
         manualChunks: {
-          vendor: ['react', 'react-dom'], // Split out common dependencies
-          // Add more vendor or large libraries here if needed
+          vendor: ['react', 'react-dom'],
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Increase chunk size limit warning if needed
+    chunkSizeWarningLimit: 3000,
+    cssCodeSplit: true,  // Enable CSS code-splitting if needed
   },
 });
