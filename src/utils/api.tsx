7e04df9dd7e85fe2c9f725e691e5e87e9733305b
FaurIonut <162836427+FaurIonut@users.noreply.github.com>
2024-08-26 00:32:36 +0300

    .

--- src/utils/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const api = axios.create({
-    baseURL: `https://109.237.99.151:5000/api`,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-
-  export default api;
